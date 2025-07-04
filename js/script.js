7d374f3edd34f0d02d71691883111b6ed93b09e5
Baljeet Matta <baljeetmatta@Baljeets-MacBook-Air.local>
2025-07-04 10:06:46 +0530

    Day 2

---
+++ js/script.js
@@ -0,0 +1,101 @@
+ "use strict"
+            // a=10;
+            // console.log(typeof a);
+
+            // a="Hello";
+            // console.log(typeof a);
+           // console.log(testData);
+/*
+           let testData=10;
+            console.log(testData);
+
+
+            testData=20;
+            console.log(testData);
+const data=10;
+//data=20;
+*/
+/* day 2*/
+//var data='This is a string';
+//console.log(typeof data);
+var result=20;
+var data="Name"
+//var message="the "+data+" of "+result+" is correct";
+//var message ="the "+data+" of "+result+" is correct";
+var message =`the ${data} of ${result} is correct`;
+message="This is a string";
+
+// console.log(message.length);
+// message=  message.toLowerCase();
+// var pos= message.indexOf("s");//0 based position, -1
+
+// console.log(pos);
+// pos=message.indexOf("x",pos+1);
+// console.log(pos);
+// pos=message.lastIndexOf("s");
+// console.log(pos);
+// console.log(message.charAt(0));
+//console.log(message.substring(2,4));
+//console.log(message.startsWith("Thi"));
+//operators-Airthmetic, Relational , Logical, assignment, comparison, modulo, bitwise
+//let a=10;
+//let b="10";
+//console.log(a===b);
+//let c=a==b;
+//int c=a==b;
+/*
+
+if(true)
+printf("hello")
+*/
+// if(a===b)
+//     console.log("Hello");
+/*
+    Control Structures
+    1. Sequence Control
+    2. Transfer of Control/Selection/Decision
+        if if else else if switch ?:
+    3.Loop Control/Iteration Logic /repetitive logic
+      do-while, for, while, break, continue
+
+      ///prototype/signature
+      returntype funname(argments type)
+      int sum(int,int);
+      int sum(int x, int y)
+      {
+      }
+*/
+
+//display();
+//console.log(display());
+let x=display();
+console.log(x);
+
+function display()
+{
+    console.log("Hello");
+    return 1;
+    
+
+}
+function sum()
+{
+    //console.log(arguments);
+//console.log(arguments[0])
+   // console.log(x,y);
+}
+
+sum(10,20,30);
+
+
+
+
+
+
+
+
+
+
+//console.log(message);
+
+
